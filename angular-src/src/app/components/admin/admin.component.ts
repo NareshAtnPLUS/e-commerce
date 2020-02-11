7e328d2838d4844ea8fb9df88b7c85a8a664696f
Naresh Kumar <nareshkumarAtnPLUS@gmail.com>
2020-02-11 14:23:29 +0530

    Added admin file

--- angular-src/src/app/components/admin/admin.component.ts
+++ angular-src/src/app/components/admin/admin.component.ts
@@ -7,52 +7,9 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
-  mobileForm = this.fb.group({
-    general:this.fb.group({
-      brand:['',Validators.required],
-      modelName:['',Validators.required],
-      modelNumber:['',Validators.required],
-      variant:this.fb.array([this.createVariant()])
-    }),
-    displayFeatures:this.fb.group({
-      size:[0,Validators.required],
-      resoluion:['',Validators.required],
-    }),
-    osAndProcessor:this.fb.group({
-      os:[0,Validators.required],
-      psrType:['',Validators.required],
-      psrBrand:['',Validators.required],
-      core:['',Validators.required],
-    }),
-    expandableMemory:[0,Validators.required],
-    camera:this.fb.group({
-      primaryCamera:['',Validators.required],
-      secondaryCamera:['',Validators.required],  
-    }),
-    networkFeatures:this.fb.group({
-      type:['',Validators.required],
-      internetConnectivity:['',Validators.required],
-    }),
-    dimension:this.fb.group({
-      width:[0,Validators.required],
-      height:[0,Validators.required],
-      depth:[0,Validators.required],
-      weight:[0,Validators.required],
-    }),
-    brandWarranty:this.fb.group({
-      brandWarranty:[0,Validators.required],  
-    })
-  })
+  
   constructor(private fb: FormBuilder) { }
-  createVariant(){
-    return this.fb.group({
-      price: [0,Validators.required],
-      available: [0,Validators.required],
-      internalStorage: [0,Validators.required],
-      color:['',Validators.required],
-      ram:[0,Validators.required]
-    });
-  }
+  
   ngOnInit() {
   }
 
